2c628e724b571ee5ded205681371b48a9f5487e8
stivan622 <stivan.tr@gmail.com>
2017-05-23 21:02:24 +0900

    slack-bot

--- Gemfile
+++ Gemfile
@@ -54,3 +54,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'line-bot-api', github: 'stivan622/line-bot-sdk-ruby'
 gem 'dotenv-rails'
+gem 'slack-api'
--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -5,18 +5,24 @@ class LinebotController < ApplicationController
   def callback
     body = request.body.read
 
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
     events = client.parse_events_from(body)
     events.each { |event|
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
+
           message = {
             type: 'text',
-            text: event.message['text']
+            text: 'slackに送信しました'
           }
+          Slack.chat_postMessage text: event.message['text'], username: "line-bot", channel: "#general"
           response = client.reply_message(event['replyToken'], message)
-          p response
         end
       end
     }
